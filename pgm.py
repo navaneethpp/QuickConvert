b0d9a38a674b730f56b9a5f13d01001e3a6a3693
navaneethpp <appunavaneeth0@gmail.com>
2024-06-08 18:57:58 +0530

    Added more conversions

--- pgm.py
+++ pgm.py
@@ -2,11 +2,14 @@
     Name: QuickConverter
     Description: A simple and efficient tool for converting length units.
     Date of Creation: 06/06/2024
-    Last Modified On: 07/06/2024
+    Last Modified On: 08/06/2024
     Creator: Navaneeth P P
     GitHub Profile: navaneethpp
-    Requirements: None
-    Notes: Not completed
+    Requirements: Python 3.6
+    Notes: Not completed,
+            Weight - Gram
+    To do:  * Selection Display
+            * Other conversions
 """
 
 def from_meter(measured_value):
@@ -209,7 +212,6 @@ def from_square_millimeter(measured_value):
     print("Square Meter: ", measured_value / 1000000)
     print("Square Kilometer: ", measured_value / pow(10, 12))
     print("Square Centimeter: ", measured_value / 100)
-    # print("Square Millimeter: ", measured_value * 100)
     print("Square Micrometer: ", measured_value * 10 ** 6)
     print("Hectare: ", measured_value / 10 ** 10)
     print("Square Mile: ", measured_value / 2.58998811 * 10 ** 12)
@@ -219,10 +221,160 @@ def from_square_millimeter(measured_value):
     print("Acre: ", measured_value / 40468564.2 * 10 ** 9)
     
 
+def from_square_micrometer(measured_value):
+    print("Square Meter: ", (measured_value / (10 ** 12)) * (10 ** -6))
+    print("Square Kilometer: ", (measured_value * (10 ** 12)) * (10 ** -6))
+    print("Square Centimeter: ", measured_value / (10 ** 8))
+    print("Square Millimeter: ", measured_value / (10 ** 6))
+    print("Hectare: ", measured_value / (10 ** 16))
+    print("Square Mile: ", measured_value / (2.59 * (10 **12)))
+    print("Square Yard: ", measured_value / (8.361 * (10 ** 6)))
+    print("Square Foot: ", measured_value / (9.290 * ( 10 ** 6)))
+    print("Square Inch: ", measured_value / (645.16 * (10 ** 6)))
+    print("Acre: ", measured_value / 4046860000)
+    
+
+def from_hectare(measured_value):
+    print("Square Meter: ", measured_value * 10000)
+    print("Square Kilometer: ", measured_value * 0.01)
+    print("Square Centimeter: ", measured_value * 100000000)
+    print("Square Millimeter: ", measured_value * 10000000000)
+    print("Square Micrometer: ", measured_value * 10000000000000)
+    print("Square Mile: ", measured_value * (0.01 / 2.58999))
+    print("Square Yard: ", measured_value * 11959.9)
+    print("Square Foot: ", measured_value * 107639)
+    print("Square Inch: ", measured_value * 15500031)
+    print("Acre: ", measured_value * 2.47105)
+    
+
+def from_square_mile(measured_value):
+    print("Square Meter: ", measured_value * 2.59 * (10 ** 6))
+    print("Square Kilometer: ", measured_value * 2.58999)
+    print("Square Centimeter: ", measured_value * 2.58999 * (10 ** 10))
+    print("Square Millimeter: ", measured_value * 2.58999 * (10 ** 12))
+    print("Square Micrometer: ", measured_value * 2.58999 * (10 **18)) 
+    print("Hectare: ", measured_value * 259)
+    print("Square Yard: ", measured_value * 3097600)
+    print("Square Foot: ", measured_value * 27878400)
+    print("Square Inch: ", measured_value * 4.0144896 * (10 ** 9))
+    print("Acre: ", measured_value * 640)
+    
+def from_square_yard(measured_value):
+    print("Square Meter: ", measured_value * 0.836127)
+    print("Square Kilometer: ", measured_value * 8.3613 * (10 ** -7))
+    print("Square Centimeter: ", measured_value * 8361.27)
+    print("Square Millimeter: ", measured_value * 836127)
+    print("Square Micrometer: ", measured_value * 836127000000) 
+    print("Hectare: ", measured_value * 0.0000836127)
+    print("Square Mile: ", measured_value * 3.2283 * (10 ** -7))
+    print("Square Foot: ", measured_value * 9)
+    print("Square Inch: ", measured_value * 1296)
+    print("Acre: ", measured_value * 0.000206612)
+    
+def from_square_foot(measured_value):
+    print("Square Meter: ", measured_value * 0.092903)
+    print("Square Kilometer: ", measured_value * 9.2903 * (10 ** -8))
+    print("Square Centimeter: ", measured_value * 929.0304)
+    print("Square Millimeter: ", measured_value * 92903.04)
+    print("Square Micrometer: ", measured_value * 92903040000) 
+    print("Hectare: ", measured_value * 9.2903 * (10 ** -6))
+    print("Square Mile: ", measured_value * 3.587 * (10 ** -8))
+    print("Square Yard: ", measured_value * 0.111111)
+    print("Square Inch: ", measured_value * 144)
+    print("Acre: ", measured_value * 2.2957 * (10 ** -5))
+    
+
+def from_square_inch(measured_value):
+    print("Square Meter: ", measured_value * 0.00064516)
+    print("Square Kilometer: ", measured_value * 6.4516 * (10 ** -10))
+    print("Square Centimeter: ", measured_value * 6.4516)
+    print("Square Millimeter: ", measured_value * 645.16)
+    print("Square Micrometer: ", measured_value * 645160000) 
+    print("Hectare: ", measured_value * 6.4516 * (10 ** -8))
+    print("Square Mile: ", measured_value * 2.491 * (10 ** -10))
+    print("Square Yard: ", measured_value * 0.00694444)
+    print("Square Foot: ", measured_value * 9)
+    print("Acre: ", measured_value * 1.5942 * (10 ** -7))
+    
+    
+def from_acre(measured_value):
+    print("Square Meter: ", measured_value * 4046.86)
+    print("Square Kilometer: ", measured_value * 0.00404686)
+    print("Square Centimeter: ", measured_value * 40468600)
+    print("Square Millimeter: ", measured_value * 4046860000)
+    print("Square Micrometer: ", measured_value * 4046860000000) 
+    print("Hectare: ", measured_value * 0.404686)
+    print("Square Mile: ", measured_value * 0.0015625)
+    print("Square Yard: ", measured_value * 4840)
+    print("Square Foot: ", measured_value * 43560)
+    print("Square Inch: ", measured_value * 6272640)
+    
+def from_cubic_meter(measured_value):
+    print("Cubic Kilometer: ", (measured_value * 1 * (10 ** -9)))
+    print("Cubic Millimeter: ", (measured_value * 1 * (10 ** 9)))
+    print("Liter: ", measured_value * 1000)
+    print("Milliliter: ", (measured_value * 1 * (10 ** 6)))
+    print("US Gallon: ", measured_value * 264.172)
+    
+
+def from_cubic_kilometer(measured_value):
+    print("Cubic Meter: ", measured_value * 1 * (10 ** 9))
+    print("Cubic Millimeter: ", (measured_value * 1 * (10 ** 18)))
+    print("Liter: ", measured_value * 1 * (10 ** 12))
+    print("Milliliter: ", (measured_value * 1 * (10 ** 15)))
+    print("US Gallon: ", measured_value * 2.64172 * (10 ** 11))
+    
+    
+def from_cubic_millimeter(measured_value):
+    print("Cubic Meter: ", measured_value * 1 * (10 ** -9))
+    print("Cubic Kilometer: ", (measured_value * 1 * (10 ** -18)))
+    print("Liter: ", measured_value * 1 * (10 ** -9))
+    print("Milliliter: ", (measured_value * 1 * (10 ** -3)))
+    print("US Gallon: ", measured_value * 2.64172 * (10 ** -7))
+    
+def from_liter(measured_value):
+    print("Cubic Meter: ", measured_value * 0.001)
+    print("Cubic Kilometer: ", (measured_value * 1 * (10 ** -12)))
+    print("Cubic Millimeter: ", measured_value * 1 * (10 ** 6))
+    print("Milliliter: ", (measured_value * 1000))
+    print("US Gallon: ", measured_value * 0.264172)
+    
+def from_milliliter(measured_value):
+    print("Cubic Meter: ", measured_value * 1 * (10 ** -6))
+    print("Cubic Kilometer: ", (measured_value * 1 * (10 ** -15)))
+    print("Cubic Millimeter: ", measured_value * 1000)
+    print("Liter: ", measured_value * 0.001)
+    print("US Gallon: ", measured_value * 0.000264172)
+    
+def from_us_gallon(measured_value):
+    print("Cubic Meter: ", measured_value * 0.00378541)
+    print("Cubic Kilometer: ", (measured_value * 3.78541 * (10 ** -12)))
+    print("Cubic Millimeter: ", measured_value * 3.78541 * (10 ** 6))
+    print("Liter: ", measured_value * 3.78541)
+    print("Milliliter: ", (measured_value * 3,785.41))
+    
+def from_kilogram(measured_value):
+    print("Gram: ", measured_value * 1000)
+    print("Milligram: ", measured_value * 1 * (10 ** 6))
+    print("Metric Ton: ", measured_value * 0.001)
+    print("Pound: ", measured_value * 2.20462)
+    print("Carrat: ", measured_value * 5000)
+    print("Atomic Mass Unit: ", measured_value * 6.02214179 * (10 ** 26))
+    
+def from_gram(measured_value):
+    print("Kilogram", measured_value * 0.001)
+    # print("Gram: ", measured_value * 1000)
+    print("Milligram: ", measured_value * 1000)
+    print("Metric Ton: ", measured_value * 1 * (10 ** -6))
+    print("Pound: ", measured_value * 0.00220462)
+    print("Carrat: ", measured_value * 5)
+    print("Atomic Mass Unit: ", measured_value * 6.02214179 * (10 ** 23))
+    
+
 def length():
     print()
     print("Choose measured value unit")
-    print("1. Meter\n2. Kilometer\n3. Centimeter\n4. Millimeter\n5. Micrometer\n6. Nanometer\n7. Mile\n8. Yard\n9. Foot\n10. Inch\n11. Light Year\n")
+    print("1. Meter\n2. Kilometer\n3. Centimeter\n4. Millimeter\n5. Micrometer\n6. Nanometer\n7. Mile\n8. Yard\n9. Foot\n10. Inch\n11. Light Year\n0. Exit\n0. Exit\n")
     measured_unit = int(input("Enter the value: "))
 
     print()
@@ -258,7 +410,7 @@ def length():
 def temperature():
     print()
     print("Choose measured value unit")
-    print("1. Celsius\n2. Kelvin\n3. Fahrenheit")
+    print("1. Celsius\n2. Kelvin\n3. Fahrenheit\n0. Exiting")
     measured_unit = int(input("Enter the value: "))
     print()
     measured_value = float(input("Enter the measurement: "))
@@ -276,7 +428,7 @@ def temperature():
 def area():
     print()
     print("Choose measured value unit")
-    print("1. Square Meter\n2. Square Kilometer\n3. Square Centimeter\n4. Square Millimeter\n5. Square Micrometer\n6. Hectare\n7. Square Mile\n8. Square Yard\n9. Square Foot\n9. Square Inch\n10. Acre")
+    print("1. Square Meter\n2. Square Kilometer\n3. Square Centimeter\n4. Square Millimeter\n5. Square Micrometer\n6. Hectare\n7. Square Mile\n8. Square Yard\n9. Square Foot\n10. Square Inch\n11. Acre\n0. Exiting...")
     measured_unit = int(input("Enter the value: "))
     print()
     measured_value = float(input("Enter the measurement: "))
@@ -289,18 +441,85 @@ def area():
         from_square_centimeter(measured_value)
     elif (measured_unit == 4):
         from_square_millimeter(measured_value)
+    elif (measured_unit == 5):
+        from_square_micrometer(measured_value)
+    elif (measured_unit == 6):
+        from_hectare(measured_value)
+    elif (measured_unit == 7):
+        from_square_mile(measured_value)
+    elif (measured_unit == 8):
+        from_square_yard(measured_value)
+    elif (measured_unit == 9):
+        from_square_foot(measured_value)
+    elif (measured_unit == 10):
+        from_square_inch(measured_value)
+    elif (measured_unit == 11):
+        from_acre(measured_value)
+    elif (measured_value == 0):
+        print("Exiting...")
+    else:
+        print("Error occurred. Please try again!")
+        
+        
+def volume():
+    print()
+    print("Choose measured value unit")
+    print("1. Cubic Meter\n2. Cubic Kilometer\n3. Cubic Millimeter\n4. Liter\n5. Milliliter\n6. US Gallon\n0. Exit")
+    measured_unit = int(input("Enter the value: "))
+    print()
+    measured_value = float(input("Enter the measurement: "))
     
+    if measured_unit == 1:
+        from_cubic_meter(measured_value)
+    elif measured_unit == 2:
+        from_cubic_kilometer(measured_value)
+    elif measured_unit == 3:
+        from_cubic_millimeter(measured_value)
+    elif measured_unit == 4:
+        from_liter(measured_value)
+    elif measured_unit == 5:
+        from_milliliter(measured_value)
+    elif measured_unit == 6:
+        from_us_gallon(measured_value)
+    elif measured_unit == 0:
+        print("Exiting...")
+    else:
+        print("Error Occurred")
+        
+
+def weight():
+    print()
+    print("Choose measured value unit")
+    print("1. Kilogram\n2. Gram\n3. Milligram\n4. Metric Ton\n5. Pound\n6. Carrat\n7. Atomic Mass Unit\n0. Exit")
+    measured_unit = int(input("Enter the measurement: "))
+    print()
+    measured_value = float(input("Enter the measurement: "))
+    
+    if (measured_unit == 1):
+        from_kilogram(measured_value)
+    elif (measured_unit == 2):
+        from_gram(measured_value)
+    elif (measured_unit == 0):
+        print("Exiting...")
+    else:
+        print("Error Occurred. Please try again!")
     
     
     
 
 print("Choose correct option by the number")
-print("1. Length\n2. Temperature\n3. Area\n4. Volume\n5. Weight\n6. Time")
+print("1. Length\n2. Temperature\n3. Area\n4. Volume\n5. Weight\n6. Time\n0. Exit")
 option = int(input("Enter your choice: "))
 
 if option == 1:
     length()
-if option == 2:
+elif option == 2:
     temperature()
-if option == 3:
+elif option == 3:
     area()
+elif option == 4:
+    volume()
+elif option == 5:
+    weight()
+elif option == 0:
+    print("Exiting...")
